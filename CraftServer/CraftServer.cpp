a4b6bd2d291314ab96a6369851b69be6c686fca1
Taylor Payne <taylorpayne111@gmail.com>
2017-09-25 09:03:12 -0600

    Add dependecies from Craft project including config.h, world, noise, and sqlite3.  Add includes for these dependencies and WinSock.

--- CraftServer/CraftServer.cpp
+++ CraftServer/CraftServer.cpp
@@ -1,4 +1,11 @@
 // CraftServer.cpp : Defines the entry point for the console application.
+#pragma comment(lib,"ws2_32.lib")
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include "config.h"
+#include "sqlite3.h"
+#include "noise.h"
+#include "world.h"
 
 int main()
 {
